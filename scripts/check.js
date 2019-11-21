cc1e4fb80bdf2c95f1b8351d3e3a16c759eee37d
zhanghao05 <cmcesummer@126.com>
2019-11-21 20:34:26 +0800

    更新输出检查文件功能

--- scripts/check.js
+++ scripts/check.js
@@ -1,33 +1,16 @@
-const eslint = require("eslint");
 const path = require("path");
 const fs = require("fs");
+const eslint = require("eslint");
+const xlsx = require("node-xlsx");
 const glob = require("glob");
 const ignore = require("ignore");
 
 const { CLIEngine } = eslint;
 
-const cli = new CLIEngine({
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-            globalReturn: true,
-            impliedStrict: true,
-            jsx: true,
-            experimentalObjectRestSpread: true
-        }
-    },
-    parser: "babel-eslint",
-    rules: {
-        complexity: ["error", { max: 0 }]
-    },
-    useEslintrc: false
-});
-
 /**
  * 提取函数类型正则
  */
-const REG_FUNC_TYPE = /^(Method |Async function |Arrow function |Function )/g;
+const REG_FUNC_TYPE = /^(Method |Async function |Async method |Arrow function |Function |Static |Constructor )/g;
 
 /**
  * eslint提示前缀
@@ -53,8 +36,8 @@ function getMain(message) {
  */
 function getComplexity(message) {
     const main = getMain(message);
-    /(\d+)\./g.test(main);
-    return +RegExp.$1;
+    const reg = /(\d+)\./;
+    return main.match(reg)[1];
 }
 
 /**
@@ -63,8 +46,9 @@ function getComplexity(message) {
  */
 function getFunctionName(message) {
     const main = getMain(message);
-    let test = /'([a-zA-Z0-9_$]+)'/g.test(main);
-    return test ? RegExp.$1 : "*";
+    const reg = /'([a-zA-Z0-9_$]+)'/;
+    const arr = main.match(reg);
+    return arr ? arr[1] : "*";
 }
 
 /**
@@ -72,8 +56,9 @@ function getFunctionName(message) {
  * @param {*} message
  */
 function getFunctionType(message) {
-    let hasFuncType = REG_FUNC_TYPE.test(message);
-    return hasFuncType ? RegExp.$1 : "";
+    const arr = message.match(REG_FUNC_TYPE);
+    if (arr && arr[0]) return arr[0].trim();
+    return "";
 }
 
 /**
@@ -84,24 +69,37 @@ function getFileName(filePath) {
     return filePath.replace(process.cwd(), "").trim();
 }
 
-/**
- * 获取重构建议
- * @param {*} complexity
- */
-function getAdvice(complexity) {
-    if (complexity > 15) {
-        return "强烈建议";
-    } else if (complexity > 10) {
-        return "建议";
-    } else {
-        return "无需";
+function createCliEngine({ checkType, sourceType = "module", ecmaVersion = 2020 }) {
+    // sourceType?: "module" | 'script'
+    // ecmaVersion?: 3 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 2015 | 2016 | 2017 | 2018 | 2019 | 2020;
+    let parser = "babel-eslint";
+    if (checkType === "typescript") {
+        parser = "@typescript-eslint/parser";
     }
+    return new CLIEngine({
+        parserOptions: {
+            ecmaVersion,
+            sourceType,
+            ecmaFeatures: {
+                globalReturn: true,
+                impliedStrict: true,
+                jsx: true,
+                experimentalObjectRestSpread: true
+            }
+        },
+        parser,
+        rules: {
+            complexity: ["error", { max: 0 }]
+        },
+        useEslintrc: false
+    });
 }
 
 /**
  * 获取单个文件的复杂度
  */
 function executeOnFiles(paths, min) {
+    const cli = createCliEngine();
     const reports = cli.executeOnFiles(paths).results;
     const result = [];
     const fileCount = paths.length;
@@ -120,8 +118,7 @@ function executeOnFiles(paths, min) {
                         funcName: getFunctionName(message),
                         position: line + "," + column,
                         fileName: getFileName(filePath),
-                        complexity,
-                        advice: getAdvice(complexity)
+                        complexity
                     });
                 }
             }
@@ -130,17 +127,10 @@ function executeOnFiles(paths, min) {
     return { fileCount, funcCount, result };
 }
 
-const EXTENSIONS = "**/*.js";
-
 /**
  * 默认忽略文件夹
  */
 const DEFAULT_IGNORE_PATTERNS = ["node_modules/**", "build/**", "dist/**", "output/**", "common_build/**"];
-
-/**
- * ignore文件名
- */
-const IGNORE_FILE_NAME = ".gitignore";
 
 /**
  * 默认参数
@@ -149,8 +139,8 @@ const DEFAULT_PARAM = {
     rootPath: "",
     ignoreRules: [],
     defalutIgnore: true,
-    extensions: EXTENSIONS,
-    ignoreFileName: IGNORE_FILE_NAME
+    extensions: "**/*.js",
+    ignoreFileName: ".gitignore"
 };
 
 /**
@@ -222,7 +212,7 @@ async function scan(param) {
 
 /**
  * 执行扫描
- * @param {*} scanParam 扫描参数，具体参见 c-scan
+ * @param {*} scanParam 扫描参数，DEFAULT_PARAM
  * @param {*} min 最小代码复杂度 , 大于此值不会被添加到结果
  */
 async function check(scanParam = {}, min = 1) {
@@ -230,26 +220,61 @@ async function check(scanParam = {}, min = 1) {
     return executeOnFiles(files, min);
 }
 
-async function showFil() {
-    const checkNumber = 6;
-    const files = await check(
-        {
-            // extensions: "**/*.js",
-            rootPath: path.join(__dirname, "../src/")
-            // defalutIgnore: "true",
-            // ignoreRules: [],
-            // ignoreFileName: ".gitignore"
-        },
-        checkNumber
-    );
+/**
+ * 转化数据格式
+ * @param {*} result eslint检查结果
+ */
+function tarnformData(result) {
+    const keyArray = ["funcType", "funcName", "fileName", "position", "complexity"];
+    const data = [["函数类型", "函数名", "文件名", "代码位置", "复杂度"]];
+    for (const item of result) {
+        data.push(keyArray.map(key => item[key]));
+    }
+    return data;
+}
+
+/**
+ * 生成excel文件
+ * @param {*} path 生成的文件位置
+ * @param {*} data 生成数据， buffer
+ * @param {*} name sheet名称
+ */
+function createExcel(path, data, name = "Sheet1") {
+    const buffer = xlsx.build([{ name, data: data }]);
+    return new Promise((res, rej) => {
+        fs.writeFile(path, buffer, "utf-8", er => {
+            if (er) {
+                rej(er);
+                return;
+            }
+            res();
+        });
+    });
+}
+
+/**
+ * 执行函数
+ * @param {*} scanParam 扫描参数，DEFAULT_PARAM
+ * @param {*} min 限制的复杂度
+ */
+async function exec(scanParam, min = 10) {
+    const files = await check(scanParam, min);
     console.log(`检查文件数：${files.fileCount}`);
     console.log(`检查函数数：${files.funcCount}`);
     console.log(`圈复杂度检查结果：`);
-    if (files.result.length > 0) {
-        console.log(files.result);
-    } else {
-        console.log(`-无函数复杂度大于${checkNumber}`);
+    const length = files.result.length;
+    if (length === 0) {
+        console.log(`没有函数圈复杂度大于${min}`);
+        return;
     }
+    if (files.result.length <= 10) {
+        console.log(files.result);
+        return;
+    }
+    const filename = path.join(__dirname, "./advice-excel.xlsx");
+    await createExcel(filename, tarnformData(files.result));
+    console.log(`复杂度越小越好，因此建议修改复杂度超过 ${min} 的函数。`);
+    console.log(`建议修改函数数量：${files.result.length};  详情请查阅文件: ${filename}`);
 }
 
-showFil();
+exec({ rootPath: path.join(__dirname, "../src/"), extensions: "**/*.?(js|ts|tsx)" }, 6);
