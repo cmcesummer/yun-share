e8c19d0cc2fb9beef08d6c154404c42d0dceb1e7
cmce <cmcesummer@126.com>
2020-01-29 20:45:50 +0800

    更新mac适应

--- app/mainThread/global.js
+++ app/mainThread/global.js
@@ -1,3 +1,5 @@
 const { SAVE_FILE_DIR } = require("./rpc");
 
 global.SAVE_FILE_DIR = SAVE_FILE_DIR;
+
+global.APP_PLATFORM_WINDOWS = process.platform === "win32";
--- src/skeleton/Header.js
+++ src/skeleton/Header.js
@@ -3,7 +3,7 @@ import { Icon } from "antd";
 import BaseComponent from "../components/BaseComponent";
 import bind from "../Hoc/bind";
 
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, remote } = require("electron");
 
 @bind("Header", { loading: false })
 class Header extends BaseComponent {
@@ -33,15 +33,19 @@ class Header extends BaseComponent {
                     <div className="bug" onClick={this.debug}>
                         <Icon type="bug" />
                     </div>
-                    <div className="min" onClick={this.min}>
-                        <Icon type="minus" />
-                    </div>
+                    {remote.getGlobal("APP_PLATFORM_WINDOWS") ? (
+                        <>
+                            <div className="min" onClick={this.min}>
+                                <Icon type="minus" />
+                            </div>
+                            <div className="close" onClick={this.close}>
+                                <Icon type="close" className="icon" />
+                            </div>
+                        </>
+                    ) : null}
                     {/* <div className="max" onClick={this.max}>
                         <Icon type="fullscreen" />
                     </div> */}
-                    <div className="close" onClick={this.close}>
-                        <Icon type="close" className="icon" />
-                    </div>
                 </div>
             </header>
         );
